45f6355acde9e763903e8f54a7004bc9b852432b
Saikat Sikder <saikatsikder2911@gmail.com>
2026-08-22 17:56:58 +0600

    changes

--- app/routes/url.go
+++ app/routes/url.go
@@ -6,13 +6,11 @@ import (
 )
 
 func ApiRouters(r chi.Router) {
-	// API Endpoints
 	r.Route("/api/v1", func(r chi.Router) {
 		r.Get("/health", handler.ServerHealth)
 		r.Post("/url", handler.GenerateShortCode)
 	})
 
-	// Redirect Endpoint (Supports both GET and HEAD for curl -I / browsers)
 	r.Get("/{code}", handler.RedirectURL)
 	r.Head("/{code}", handler.RedirectURL)
 }
